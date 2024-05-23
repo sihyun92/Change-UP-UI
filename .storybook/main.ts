5b15af5309bb4f1f13b9ff7cb1289d31f279a589
pildrums <there7788@gmail.com>
2024-05-23 23:35:02 +0900

    Init: Storybook 엔트리 파일 생성

---
+++ .storybook/main.ts
@@ -0,0 +1,30 @@
+import type { StorybookConfig } from "@storybook/react-webpack5";
+
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/preset-create-react-app",
+    "@storybook/addon-onboarding",
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@chromatic-com/storybook",
+    "@storybook/addon-interactions",
+    "@storybook/addon-actions",
+  ],
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+  staticDirs: ["../public"],
+  typescript: {
+    check: false,
+    checkOptions: {},
+    reactDocgen: "react-docgen",
+    reactDocgenTypescriptOptions: {},
+    skipCompiler: true,
+  },
+};
+export default config;
