832417c0b2aba6a1a6d7f1650c39688f3dd4c948
pildrums <there7788@gmail.com>
2024-05-27 16:27:19 +0900

    Fix: globalStyles.scss 적용

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import "./styles/globalStyles.scss";
 import reportWebVitals from "./reportWebVitals";
 import App from "./App";
 
