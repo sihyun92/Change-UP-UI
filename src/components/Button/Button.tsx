497ce820c27314f65363b5787244a0d92641eb75
pildrums <there7788@gmail.com>
2024-07-04 15:41:38 +0900

    Feature: hover, disabled, fontSize, padding, borderRadius, border, shadow props값으로 커스터마이징하는 기능 추가
    - hover: hoverBorder, hoverColor, hoverBackground
    - disabled: disabledBorder, disabledColor, disabledBackground
    - hover는 CSSProperties에서 수정 불가능 -> useState에 boolean 값으로 변경하는 방식으로 처리
    - disabled의 경우에도 상기한 방법으로 불가능 -> ButtonHTMLAttributes 안에 정의되어 있는 disabled 값을 이용해 조건부 렌더링 처리

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,42 +1,66 @@
-import { ButtonHTMLAttributes } from "react";
+import { CSSProperties, useCallback, useState } from "react";
 import styles from "./Button.module.scss";
 import classNames from "classnames/bind";
+import { ButtonProps } from "@/lib/interface/button";
 
 const cx = classNames.bind(styles);
 
-export type ButtonSize = "small" | "medium" | "large";
-export type ButtonColor =
-  | "primary"
-  | "secondary"
-  | "success"
-  | "failure"
-  | "cancel";
-export type ButtonOutline = boolean;
-
-export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  size?: ButtonSize;
-  color?: ButtonColor;
-  outline?: ButtonOutline;
-}
-
 function Button({ ...rest }: ButtonProps) {
+  // hook & function
+  const [hover, setHover] = useState(false);
+  const onMouseEnter = useCallback(() => {
+    setHover(true);
+  }, []);
+  const onMouseLeave = useCallback(() => {
+    setHover(false);
+  }, []);
+
   // primary outline className
   const primaryOutline = `${
-    rest.outline ? (rest.color === "primary" ? "primary-outline" : "") : ""
+    rest.buttonOutline
+      ? rest.buttonColor === "primary"
+        ? "primary-outline"
+        : ""
+      : ""
   }`;
   // secondary outline className
   const secondaryOutline = `${
-    rest.outline ? (rest.color === "secondary" ? "secondary-outline" : "") : ""
+    rest.buttonOutline
+      ? rest.buttonColor === "secondary"
+        ? "secondary-outline"
+        : ""
+      : ""
   }`;
+
+  // dynamicStyle
+  const dynamicStyle: CSSProperties = {
+    color:
+      (hover ? rest.hoverColor : rest.color) &&
+      (rest.disabled ? rest.disabledColor : rest.color),
+    background:
+      (hover ? rest.hoverBackground : rest.background) &&
+      (rest.disabled ? rest.disabledBackground : rest.background),
+    fontSize: rest.fontSize,
+    padding: rest.padding,
+    borderRadius: rest.radius,
+    border:
+      (hover ? rest.hoverBorder : rest.border) &&
+      (rest.disabled ? rest.disabledBorder : rest.border),
+  };
 
   return (
     <button
+      style={dynamicStyle}
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
       className={cx(
-        "block",
-        `${rest.size}`,
-        `${rest.color}`,
+        `block`,
+        `${rest.shape}`,
+        `${rest.buttonSize}`,
+        `${rest.buttonColor}`,
         `${primaryOutline}`,
         `${secondaryOutline}`,
+        `${rest.shadow && "shadow"}`,
       )}
       {...rest}
     />
